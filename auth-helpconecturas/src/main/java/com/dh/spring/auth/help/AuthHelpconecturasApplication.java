fb80f3f2fd2f6b0a798284dbfe29b3efba57ea22
Cicero Lopes da Silv <Cicero Lopes da Silv@DESKTOP-A739V4B>
2022-08-20 13:20:10 -0300

    project creation

---
+++ auth-helpconecturas/src/main/java/com/dh/spring/auth/help/AuthHelpconecturasApplication.java
@@ -0,0 +1,13 @@
+package com.dh.spring.auth.help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthHelpconecturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthHelpconecturasApplication.class, args);
+	}
+
+}
---
+++ auth-helpconecturas/src/test/java/com/dh/spring/auth/help/AuthHelpconecturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.spring.auth.help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthHelpconecturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
